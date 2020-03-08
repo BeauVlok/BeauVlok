a0c39807c749ed1f62940d34428c492f7b52f534
Joris van der Sluis <beauvlok@gmail.com>
2020-03-08 19:26:21 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.20fedb9fad46a44c1cbe8a30643525b1.js"
+  "/precache-manifest.10070d4ca59f7fafdb5559203cce7676.js"
 );
 
 workbox.clientsClaim();
