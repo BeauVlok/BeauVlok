c75c755e53474a6115f67d8c5acb802d528f0ae7
JorisvdSluis <0946664@hr.nl>
2020-06-21 13:33:12 +0200

    added pfd

--- website/src/Pages/Projects.js
+++ website/src/Pages/Projects.js
@@ -1,23 +1,19 @@
 import React from "react";
-import { Grid, Segment, Header, Divider } from "semantic-ui-react";
+import { Grid, Segment, Header, Divider, Button } from "semantic-ui-react";
 import "./Projects.css";
 import Acute from "../Text/Acute";
 import Wound from "../Text/Wound";
 import Extern from "../Text/Extern";
+import participation from "../assets/bewijs.pdf";
 class Projects extends React.Component {
-
   render() {
-
     return (
       <Segment style={{ padding: this.props.padding }} vertical>
         <Grid container stackable verticalAlign="middle">
           <Grid.Row>
-          <Header
-                as="h3"
-                style={{ fontSize: "2em", color: "midnightblue" }}
-              >
-                Bevorderende activiteiten
-              </Header>
+            <Header as="h3" style={{ fontSize: "2em", color: "midnightblue" }}>
+              Bevorderende activiteiten
+            </Header>
             <p>
               Een leven lang leren begint bij mezelf en een belangrijk onderdeel
               hierin is het volgen van klinische lessen, bijscholing, workshops
@@ -26,14 +22,31 @@ class Projects extends React.Component {
               workshops waarin in kon oriënteren en ontwikkelen in wat ik wil in
               de toekomst als verpleegkundige. Ook heb ik twee activiteiten
               buiten school gevolgd, zodat ik ook in de praktijk kon zien wat ik
-              interessant vind en welke ontwikkeling ik hiervoor kan maken.{" "}
+              interessant vind en welke ontwikkeling ik hiervoor kan maken.
             </p>
+            <div
+              style={{ width: "100%", paddingTop: "1em", paddingBottom: "3em" }}
+            >
+              <a href={participation} target="_blank">
+                <Button
+                  fluid
+                  style={{
+                    backgroundColor: "midnightblue",
+                    color: "white",
+                    width: "60%",
+                    marginLeft: "20%",
+                  }}
+                >
+                  Bewijs deelname activiteiten
+                </Button>
+              </a>
+            </div>
             <Divider />
             <Acute />
             <Divider />
             <Wound />
-            <Divider/>
-            <Extern/>
+            <Divider />
+            <Extern />
           </Grid.Row>
         </Grid>
       </Segment>
